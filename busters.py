77a93679e287a0809215d515e4427324863830e8
Sergio Rodriguez <150100396+srodr007@users.noreply.github.com>
2024-10-13 20:23:06 +0200

    Update busters.py

--- busters.py
+++ busters.py
@@ -1,9 +1,5 @@
 # busters.py
 # ----------
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley.
 # 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
